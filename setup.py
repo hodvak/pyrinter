8251e8b9a8f1088eeacbc57baabbe347301d0995
Hod Vaknin <hodvak@gmail.com>
2022-10-21 16:06:25 +0300

    feat: change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyrinter",
-    version="0.0.1a2",
+    version="0.0.1a3",
     packages=find_packages(include=["pyrinter", "pyrinter.*"]),
     author="Hod Vaknin",
     license="MIT",
